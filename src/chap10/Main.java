7081aa81a2fcb66dc68e31e42e47bc2e3bb588f4
Shuhei_Hiroshima <>
2018-03-23 12:06:39 +0900

    finish Chap10

--- src/chap10/Main.java
+++ src/chap10/Main.java
@@ -5,13 +5,13 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Hero hero1 = new Hero("shupo",77,77);
+		Hero hero1 = new Hero("shupo", 77, 77);
 		FileOutputStream fos = new FileOutputStream("rpgsave.dat");
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		oos.writeObject(hero1);
 		oos.flush();
 		oos.close();
-		
+
 		FileInputStream fis = new FileInputStream("rpgsave.dat");
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		Hero hero2 = (Hero) ois.readObject();
